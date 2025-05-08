fd84aa31b768304d6825320a7c71c8fe90cceabd
azaryatumundo <s22210706@student.unklab.ac.id>
2025-05-08 16:49:51 +0800

    update db

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "Kelompok7_", "peminjaman_buku");
+$conn = new mysqli("localhost", "root", "Katasandi00_", "peminjaman_buku");
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
